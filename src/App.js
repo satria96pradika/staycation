af907fa2a28c6eef86d4114cc3c45cd077a495a5
satria96pradika <idsatria96pradika@gmail.com>
2020-08-23 17:44:10 +0700

    my first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "assets/scss/style.scss";
+
+function App() {
+  return <div className="App">Hello there!</div>;
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+import moduleName from "elements/Button";
+export default class Home extends Component {
+  render() {
+    return <div></div>;
+  }
+}
