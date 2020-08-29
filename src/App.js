d16a23a6bf444f45da14bb7ef9d0556a25d3c6c1
Satria <idsatria96pradika@gmail.com>
2020-08-29 16:03:22 +0700

    create Header component and put to arrange HomePage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "assets/scss/style.scss";
+import HomePage from "pages/HomePage";
 
 function App() {
-  return <div className="App">Hello there!</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={HomePage}></Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import Header from "parts/Header";
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <>
+        <Header {...this.props}></Header>
+      </>
+    );
+  }
+}
